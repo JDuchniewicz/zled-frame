4b5a2b6317f5d0e19d928647381e98f092aa048f
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2024-01-28 19:56:23 +0100

    Basic handling of POST /api/images.
    Signed-off-by: Jakub Duchniewicz <j.duchniewicz@gmail.com>

--- zled_frame/src/network.c
+++ zled_frame/src/network.c
@@ -12,7 +12,7 @@ LOG_MODULE_REGISTER(networking); // TODO: how does it work?
 
 #if defined(CONFIG_NET_IPV4)
 K_THREAD_STACK_ARRAY_DEFINE(tcp4_handler_stack, CONFIG_NET_SAMPLE_NUM_HANDLERS,
-			    STACK_SIZE);
+							STACK_SIZE);
 static struct k_thread tcp4_handler_thread[CONFIG_NET_SAMPLE_NUM_HANDLERS];
 static k_tid_t tcp4_handler_tid[CONFIG_NET_SAMPLE_NUM_HANDLERS];
 #endif
@@ -20,7 +20,7 @@ static k_tid_t tcp4_handler_tid[CONFIG_NET_SAMPLE_NUM_HANDLERS];
 #if defined(CONFIG_NET_IPV6)
 // TODO: somehow in the original sample IPv6 is mandatory - otherwise we get a crash upon second HTTP request?
 K_THREAD_STACK_ARRAY_DEFINE(tcp6_handler_stack, CONFIG_NET_SAMPLE_NUM_HANDLERS,
-			    STACK_SIZE);
+							STACK_SIZE);
 static struct k_thread tcp6_handler_thread[CONFIG_NET_SAMPLE_NUM_HANDLERS];
 static k_tid_t tcp6_handler_tid[CONFIG_NET_SAMPLE_NUM_HANDLERS];
 #endif
@@ -42,23 +42,34 @@ static void process_tcp4(void);
 static void process_tcp6(void);
 
 K_THREAD_DEFINE(tcp4_thread_id, STACK_SIZE,
-		process_tcp4, NULL, NULL, NULL,
-		THREAD_PRIORITY, 0, -1);
-
+				process_tcp4, NULL, NULL, NULL,
+				THREAD_PRIORITY, 0, -1);
 
 K_THREAD_DEFINE(tcp6_thread_id, STACK_SIZE,
-		process_tcp6, NULL, NULL, NULL,
-		THREAD_PRIORITY, 0, -1);
+				process_tcp6, NULL, NULL, NULL,
+				THREAD_PRIORITY, 0, -1);
 
+#define RECEIVE_BUF_LEN 128
+
+static endpoint_t valid_endpoints[NUMBER_OF_ENDPOINTS] = {
+	{"/", GET}, // TODO :add handling for favicon lol
+	{"/api/image", POST},
+};
+
+#define MAX_IMAGE_SIZE (16 * 16 * 3 * sizeof(uint8_t))
+static char received_image[MAX_IMAGE_SIZE];
+static size_t rcv_img_offset;
 
 void event_handler(struct net_mgmt_event_callback *cb,
-			  uint32_t mgmt_event, struct net_if *iface)
+				   uint32_t mgmt_event, struct net_if *iface)
 {
-	if ((mgmt_event & EVENT_MASK) != mgmt_event) {
+	if ((mgmt_event & EVENT_MASK) != mgmt_event)
+	{
 		return;
 	}
 
-	if (mgmt_event == NET_EVENT_L4_CONNECTED) {
+	if (mgmt_event == NET_EVENT_L4_CONNECTED)
+	{
 		LOG_INF("Network connected");
 
 		connected = true;
@@ -67,10 +78,14 @@ void event_handler(struct net_mgmt_event_callback *cb,
 		return;
 	}
 
-	if (mgmt_event == NET_EVENT_L4_DISCONNECTED) {
-		if (connected == false) {
+	if (mgmt_event == NET_EVENT_L4_DISCONNECTED)
+	{
+		if (connected == false)
+		{
 			LOG_INF("Waiting network to be connected");
-		} else {
+		}
+		else
+		{
 			LOG_INF("Network disconnected");
 			connected = false;
 		}
@@ -83,10 +98,12 @@ void event_handler(struct net_mgmt_event_callback *cb,
 
 static ssize_t sendall(int sock, const void *buf, size_t len)
 {
-	while (len) {
+	while (len)
+	{
 		ssize_t out_len = send(sock, buf, len, 0);
 
-		if (out_len < 0) {
+		if (out_len < 0)
+		{
 			return out_len;
 		}
 
@@ -98,24 +115,27 @@ static ssize_t sendall(int sock, const void *buf, size_t len)
 }
 
 static int setup(int *sock, struct sockaddr *bind_addr,
-		 socklen_t bind_addrlen)
+				 socklen_t bind_addrlen)
 {
 	int ret;
 
 	*sock = socket(bind_addr->sa_family, SOCK_STREAM, IPPROTO_TCP);
-	if (*sock < 0) {
+	if (*sock < 0)
+	{
 		LOG_ERR("Failed to create TCP socket: %d", errno);
 		return -errno;
 	}
 
 	ret = bind(*sock, bind_addr, bind_addrlen);
-	if (ret < 0) {
+	if (ret < 0)
+	{
 		LOG_ERR("Failed to bind TCP socket %d", errno);
 		return -errno;
 	}
 
 	ret = listen(*sock, MAX_CLIENT_QUEUE);
-	if (ret < 0) {
+	if (ret < 0)
+	{
 		LOG_ERR("Failed to listen on TCP socket %d", errno);
 		ret = -errno;
 	}
@@ -125,48 +145,97 @@ static int setup(int *sock, struct sockaddr *bind_addr,
 
 // add the method + endpoint choosing method:
 
-typedef enum {
-    GET = 0,
-    POST,
-    UNKNOWN,
-} method_t;
-
 // TODO: endpoint choosing done properly - but do this later - first check the endpoint - integrate to my code and then build on top of that
 //
 // // TODO :how to handle multipackets
 
-static int parse_header(char *buf, int buf_size, char *endpoint, int endpoint_size, method_t * method)
+static int parse_header(char *buf, int buf_size, char *endpoint, int endpoint_size, method_t *method)
 {
-    char *ptr = buf, *delim_pos = NULL;
-    int len = 0;
-    if (strstr(ptr, "GET")) {
-        *method = GET;
-        ptr += 4; // +1 for whitespace
-    } else if (strstr(ptr, "POST")) {
-        *method = POST;
-        ptr += 5;
-    } else {
-        LOG_ERR("Unknown method found!");
-        return -1;
-    }
+	char *ptr = buf, *delim_pos = NULL;
+	int len = 0;
+	if (strstr(ptr, "GET"))
+	{
+		*method = GET;
+		ptr += 4; // +1 for whitespace
+	}
+	else if (strstr(ptr, "POST"))
+	{
+		*method = POST;
+		ptr += 5;
+	}
+	else
+	{
+		LOG_ERR("Unknown method found!");
+		return -1;
+	}
 
-    delim_pos = strchr(ptr, ' ');
-    len = delim_pos - ptr;
-    if (len + 1 > endpoint_size) {
-        LOG_ERR("Too long endpoint name! Max allowed: %d", endpoint_size);
-        return -1;
-    }
+	delim_pos = strchr(ptr, ' ');
+	len = delim_pos - ptr;
+	if (len + 1 > endpoint_size)
+	{
+		LOG_ERR("Too long endpoint name! Max allowed: %d", endpoint_size);
+		return -1;
+	}
 
-    strncpy(endpoint, ptr, len);
-    *(endpoint + len) = '\0'; // null-terminate it
+	strncpy(endpoint, ptr, len);
+	*(endpoint + len) = '\0'; // null-terminate it
 
-    LOG_DBG("Found method of type: %d with endpoint name: %s", *method, endpoint);
+	LOG_DBG("Found method of type: %d with endpoint name: %s", *method, endpoint);
 
-    return 0;
+	return 0;
 }
 
-/// here some basic address passing woult be necessary
-/// confirming - this is the proper place - all I now need is to extract the method name and register proper endpoints in my application - for starters hardcode it
+static int handle_endpoint(int client, char *endpoint_buf, method_t method, char *buf, int buf_len)
+{
+	// GET /
+	if (strncmp(valid_endpoints[0].name, endpoint_buf, strlen(endpoint_buf)) == 0)
+	{
+		// here we don't need to check any return values and can directly return the page to render
+		LOG_ERR("Sending the data to the client!");
+		(void)sendall(client, content, sizeof(content));
+		return 0;
+	}
+	else if (strncmp(valid_endpoints[1].name, endpoint_buf, strlen(endpoint_buf)) == 0)
+	{
+		LOG_ERR("Handling POST /api/image");
+
+		// for now this will inform that we handled all
+		if (rcv_img_offset + buf_len >= MAX_IMAGE_SIZE)
+		{
+			LOG_ERR("Stop accepting - Trying to send more data than Image can accept.");
+			// TODO: also skip the header part and only get the raw data
+			// TODO: where should it be placed?
+			rcv_img_offset = 0;
+			return 0;
+		}
+
+		LOG_ERR("Position: %d", rcv_img_offset);
+
+		strncpy(received_image + rcv_img_offset, buf, buf_len);
+
+		rcv_img_offset += buf_len;
+		return -1;
+	}
+
+	/* TODO: when to use those
+		if (strstr(buf, "\r\n\r\nOK")) {
+			LOG_ERR("Received OK");
+			running_status = true;
+			want_to_quit = true;
+			k_sem_give(&quit_lock);
+			break;
+		} else if (strstr(buf, "\r\n\r\nFAIL")) {
+			LOG_ERR("Received FAIL");
+			running_status = false;
+			want_to_quit = true;
+			k_sem_give(&quit_lock);
+			break;
+		}
+	*/
+	LOG_ERR("Unknown endpoint and header!");
+	return 0;
+}
+
 static void client_conn_handler(void *ptr1, void *ptr2, void *ptr3)
 {
 	ARG_UNUSED(ptr1);
@@ -175,38 +244,44 @@ static void client_conn_handler(void *ptr1, void *ptr2, void *ptr3)
 	int client;
 	int received;
 	int ret;
-	char buf[100];
+	char buf[RECEIVE_BUF_LEN];
 
-    // my things added for handling of the endpoints - TODO: refactor it later to be more composable
-    char endpoint_buf[20];
-    bool parsed_header = false;
-    method_t method;
+	// my things added for handling of the endpoints - TODO: refactor it later to be more composable
+	char endpoint_buf[20];
+	bool parsed_header = false;
+	method_t method;
 
 	client = *sock;
 
-	do {
+	do
+	{
 		received = recv(client, buf, sizeof(buf), 0);
 
-		if (received == 0) {
+		if (received == 0)
+		{
 			/* Connection closed */
 			LOG_DBG("[%d] Connection closed by peer", client);
 			break;
-		} else if (received < 0) {
+		}
+		else if (received < 0)
+		{
 			/* Socket error */
 			ret = -errno;
 			LOG_ERR("[%d] Connection error %d", client, ret);
 			break;
 		}
 
-        LOG_DBG("[%d] Received data: %.*s", client, received, buf);
+		LOG_DBG("[%d] Received data: %.*s", client, received, buf);
 
-        if (!parsed_header) {
-            if (parse_header(buf, sizeof(buf), endpoint_buf, sizeof(endpoint_buf), &method) != 0) {
-                LOG_ERR("[%d] Could not parse header", client);
-                break;
-            }
-            parsed_header = true;
-        } // TODO: add sending the data to the proper endpoint later
+		if (!parsed_header)
+		{
+			if (parse_header(buf, sizeof(buf), endpoint_buf, sizeof(endpoint_buf), &method) != 0)
+			{
+				LOG_ERR("[%d] Could not parse header", client);
+				break;
+			}
+			parsed_header = true;
+		} // TODO: add sending the data to the proper endpoint later
 
 		/* Note that something like this strstr() check should *NOT*
 		 * be used in production code. This is done like this just
@@ -215,23 +290,16 @@ static void client_conn_handler(void *ptr1, void *ptr2, void *ptr3)
 		 * We are assuming here that the full HTTP request is received
 		 * in one TCP segment which in real life might not.
 		 */
-		if (strstr(buf, "\r\n\r\n")) { // TODO: how to check it received all data though?
+		// if (strstr(buf, "\r\n\r\n")) { // TODO: how to check it received all data though?
+		//     LOG_ERR("END OF PACKET RECEIVED");
+		//	break;
+		// }
+		if (handle_endpoint(client, endpoint_buf, method, buf, RECEIVE_BUF_LEN) == 0)
+		{
+			LOG_ERR("Handled the endpoint - exiting.");
 			break;
 		}
 	} while (true);
-
-	/* We received status from the client */
-	if (strstr(buf, "\r\n\r\nOK")) {
-		running_status = true;
-		want_to_quit = true;
-		k_sem_give(&quit_lock);
-	} else if (strstr(buf, "\r\n\r\nFAIL")) {
-		running_status = false;
-		want_to_quit = true;
-		k_sem_give(&quit_lock);
-	} else {
-		(void)sendall(client, content, sizeof(content));
-	}
 
 	(void)close(client);
 
@@ -243,8 +311,10 @@ static int get_free_slot(int *accepted)
 {
 	int i;
 
-	for (i = 0; i < CONFIG_NET_SAMPLE_NUM_HANDLERS; i++) {
-		if (accepted[i] < 0) {
+	for (i = 0; i < CONFIG_NET_SAMPLE_NUM_HANDLERS; i++)
+	{
+		if (accepted[i] < 0)
+		{
 			return i;
 		}
 	}
@@ -261,14 +331,16 @@ static int process_tcp(int *sock, int *accepted)
 	socklen_t client_addr_len = sizeof(client_addr);
 
 	client = accept(*sock, (struct sockaddr *)&client_addr,
-			&client_addr_len);
-	if (client < 0) {
+					&client_addr_len);
+	if (client < 0)
+	{
 		LOG_ERR("Error in accept %d, stopping server", -errno);
 		return -errno;
 	}
 
 	slot = get_free_slot(accepted);
-	if (slot < 0 || slot >= CONFIG_NET_SAMPLE_NUM_HANDLERS) {
+	if (slot < 0 || slot >= CONFIG_NET_SAMPLE_NUM_HANDLERS)
+	{
 		LOG_ERR("Cannot accept more connections");
 		close(client);
 		return 0;
@@ -277,7 +349,8 @@ static int process_tcp(int *sock, int *accepted)
 	accepted[slot] = client;
 
 #if defined(CONFIG_NET_IPV6)
-	if (client_addr.sin6_family == AF_INET6) {
+	if (client_addr.sin6_family == AF_INET6)
+	{
 		tcp6_handler_tid[slot] = k_thread_create(
 			&tcp6_handler_thread[slot],
 			tcp6_handler_stack[slot],
@@ -292,7 +365,8 @@ static int process_tcp(int *sock, int *accepted)
 #endif
 
 #if defined(CONFIG_NET_IPV4)
-	if (client_addr.sin6_family == AF_INET) {
+	if (client_addr.sin6_family == AF_INET)
+	{
 		tcp4_handler_tid[slot] = k_thread_create(
 			&tcp4_handler_thread[slot],
 			tcp4_handler_stack[slot],
@@ -306,16 +380,17 @@ static int process_tcp(int *sock, int *accepted)
 	}
 #endif
 
-	if (LOG_LEVEL >= LOG_LEVEL_DBG) {
+	if (LOG_LEVEL >= LOG_LEVEL_DBG)
+	{
 		char addr_str[INET6_ADDRSTRLEN];
 
 		net_addr_ntop(client_addr.sin6_family,
-			      &client_addr.sin6_addr,
-			      addr_str, sizeof(addr_str));
+					  &client_addr.sin6_addr,
+					  addr_str, sizeof(addr_str));
 
 		LOG_DBG("[%d] Connection #%d from %s",
-			client, ++counter,
-			addr_str);
+				client, ++counter,
+				addr_str);
 	}
 
 	return 0;
@@ -331,17 +406,20 @@ static void process_tcp4(void)
 	addr4.sin_port = htons(MY_PORT);
 
 	ret = setup(&tcp4_listen_sock, (struct sockaddr *)&addr4,
-		    sizeof(addr4));
-	if (ret < 0) {
+				sizeof(addr4));
+	if (ret < 0)
+	{
 		return;
 	}
 
 	LOG_DBG("Waiting for IPv4 HTTP connections on port %d, sock %d",
-		MY_PORT, tcp4_listen_sock);
+			MY_PORT, tcp4_listen_sock);
 
-	while (ret == 0 || !want_to_quit) {
+	while (ret == 0 || !want_to_quit)
+	{
 		ret = process_tcp(&tcp4_listen_sock, tcp4_accepted);
-		if (ret < 0) {
+		if (ret < 0)
+		{
 			return;
 		}
 	}
@@ -357,17 +435,20 @@ static void process_tcp6(void)
 	addr6.sin6_port = htons(MY_PORT);
 
 	ret = setup(&tcp6_listen_sock, (struct sockaddr *)&addr6,
-		    sizeof(addr6));
-	if (ret < 0) {
+				sizeof(addr6));
+	if (ret < 0)
+	{
 		return;
 	}
 
 	LOG_DBG("Waiting for IPv6 HTTP connections on port %d, sock %d",
-		MY_PORT, tcp6_listen_sock);
+			MY_PORT, tcp6_listen_sock);
 
-	while (ret == 0 || !want_to_quit) {
+	while (ret == 0 || !want_to_quit)
+	{
 		ret = process_tcp(&tcp6_listen_sock, tcp6_accepted);
-		if (ret != 0) {
+		if (ret != 0)
+		{
 			return;
 		}
 	}
@@ -377,7 +458,8 @@ void start_listener(void)
 {
 	int i;
 
-	for (i = 0; i < CONFIG_NET_SAMPLE_NUM_HANDLERS; i++) {
+	for (i = 0; i < CONFIG_NET_SAMPLE_NUM_HANDLERS; i++)
+	{
 #if defined(CONFIG_NET_IPV4)
 		tcp4_accepted[i] = -1;
 		tcp4_listen_sock = -1;
@@ -388,11 +470,13 @@ void start_listener(void)
 #endif
 	}
 
-	if (IS_ENABLED(CONFIG_NET_IPV6)) {
+	if (IS_ENABLED(CONFIG_NET_IPV6))
+	{
 		k_thread_start(tcp6_thread_id);
 	}
 
-	if (IS_ENABLED(CONFIG_NET_IPV4)) {
+	if (IS_ENABLED(CONFIG_NET_IPV4))
+	{
 		k_thread_start(tcp4_thread_id);
 	}
 }
--- zled_frame/src/network.h
+++ zled_frame/src/network.h
@@ -22,11 +22,26 @@ static const char content[] = {
     #include "served_content.html.bin.inc"
 };
 
-#define CONFIG_NET_SAMPLE_NUM_HANDLERS 4// TODO: where does it come from?
+#define CONFIG_NET_SAMPLE_NUM_HANDLERS 4// TODO: where does it come from? (2 set in the sample dummy app)
 #define MAX_CLIENT_QUEUE CONFIG_NET_SAMPLE_NUM_HANDLERS
 
 #define EVENT_MASK (NET_EVENT_L4_CONNECTED | \
 		    NET_EVENT_L4_DISCONNECTED)
 
+typedef enum {
+    GET = 0,
+    POST,
+    UNKNOWN,
+} method_t;
+
+// TODO: think of more robust way of handling it
+#define NUMBER_OF_ENDPOINTS 2
+
+typedef struct {
+    const char * name;
+    uint8_t methods; // bitmap of allowed methods
+} endpoint_t;
+
 // API
 void start_listener(void);
+
